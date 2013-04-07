f72a487bd4833e66e095a8a7a911427cceeb053e
Elisa Lou <classic.melody@gmail.com>
2013-04-07 19:28:08 +0000

    Some button logic and change scaling factor.

--- src/com/elisa/a5/view/MainActivity.java
+++ src/com/elisa/a5/view/MainActivity.java
@@ -29,6 +29,9 @@ import com.elisa.a5.R;
 import com.elisa.a5.model.AnimatorModel;
 import com.elisa.a5.model.Segment;
 
+/**
+ * Elisa Lou 20372456 Assignment 5 Android Winter '13
+ */
 public class MainActivity extends Activity {
 	private AnimatorModel model;
 	private Timer t;
@@ -36,6 +39,7 @@ public class MainActivity extends Activity {
 	private MyView myView;
 	private boolean settings = false;
 	private TimerTask myTimerTask;
+	Button fwdBtn, playBtn, backBtn, stopBtn;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -54,11 +58,23 @@ public class MainActivity extends Activity {
 		params.addRule(RelativeLayout.BELOW, R.id.back);
 		params.addRule(RelativeLayout.ABOVE, R.id.slider);
 		params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+		// params.addRule(RelativeLayout.lay)
 
 		myView = new MyView(this);
 		v.addView(myView, params);
+		this.registerControllers();
+	}
 
-		final Button fwdBtn = (Button) findViewById(R.id.fwd);
+	private void setPlayback(boolean b) {
+		fwdBtn.setEnabled(b);
+		backBtn.setEnabled(b);
+		playBtn.setEnabled(b);
+		stopBtn.setEnabled(false);
+	}
+
+	private void registerControllers() {
+
+		fwdBtn = (Button) findViewById(R.id.fwd);
 		fwdBtn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				model.increaseFrames(false);
@@ -67,7 +83,7 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		final Button backBtn = (Button) findViewById(R.id.back);
+		backBtn = (Button) findViewById(R.id.back);
 		backBtn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				model.decreaseFrames();
@@ -76,11 +92,13 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		Button playBtn = (Button) findViewById(R.id.play);
+		playBtn = (Button) findViewById(R.id.play);
 		playBtn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				playBtn.setEnabled(false);
 				backBtn.setEnabled(false);
 				fwdBtn.setEnabled(false);
+				stopBtn.setEnabled(true);
 				if (slider.getProgress() >= model.getTotalFrames()) {
 					model.gotoZero();
 					slider.setProgress(model.getFrame());
@@ -89,15 +107,15 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		Button stopBtn = (Button) findViewById(R.id.stop);
+		stopBtn = (Button) findViewById(R.id.stop);
 		stopBtn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				backBtn.setEnabled(true);
-				fwdBtn.setEnabled(true);
+				setPlayback(true);
 				model.setState(AnimatorModel.State.draw);
 			}
 		});
 
+		setPlayback(false);
 		slider.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
@@ -108,6 +126,8 @@ public class MainActivity extends Activity {
 			}
 
 			public void onStartTrackingTouch(SeekBar seekBar) {
+				model.setState(AnimatorModel.State.draw);
+				setPlayback(true);
 			}
 
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -123,6 +143,7 @@ public class MainActivity extends Activity {
 		if (fileExplore.equals("yes")) {
 			String name = prefs.getString("filename", "ohno");
 			model.loadAnimation(name);
+			this.setPlayback(true);
 			slider.setMax(model.getTotalFrames());
 			model.gotoZero();
 			slider.setProgress(model.getFrame());
@@ -168,7 +189,8 @@ public class MainActivity extends Activity {
 
 		switch (item.getItemId()) {
 		case R.id.menu_load:
-			Intent myIntent = new Intent(MainActivity.this, FileExplorerActivity.class);
+			Intent myIntent = new Intent(MainActivity.this,
+					FileExplorerActivity.class);
 			t.cancel();
 			MainActivity.this.startActivity(myIntent);
 
@@ -224,29 +246,29 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onDraw(Canvas canvas) {
 			super.onDraw(canvas);
-			@SuppressWarnings("deprecation")
-			int width = getWindowManager().getDefaultDisplay().getWidth();
-			
+
+			int width = this.getWidth();
+			int height = this.getHeight();
 			ArrayList<Segment> segments = model.getSegments();
 			if (segments.size() > 0) {
 				for (Segment s : segments) {
 					int currFrame = model.getFrame();
 					Path path = new Path();
 
-					// make sure # points is greater than 0 and that the segment
-					// is spse to be visible in the current frame
 					ArrayList<Point> transformedPoints = s
 							.getTranslates(currFrame);
 					if (transformedPoints.size() > 0) {
 						Point first = transformedPoints.get(0);
-						path.moveTo(first.x*width/720, first.y*width/720);
+						path.moveTo(first.x * width / 720, first.y * height
+								/ 452);
 
 						for (int j = 1; j < s.size(); j++) {
 							Point to = transformedPoints.get(j);
-							path.lineTo(to.x*width/720, to.y*width/720);
+							path.lineTo(to.x * width / 720, to.y * height / 452);
 						}
 						if (transformedPoints.size() == 1) {
-							path.lineTo(first.x*width/720, first.y*width/720);
+							path.lineTo(first.x * width / 720, first.y * height
+									/ 452);
 						}
 					}
 
