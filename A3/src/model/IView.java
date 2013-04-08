66f8dfd47d1299145bd8b1c2c4cdbcbac1096c50
Elisa Lou <classic.melody@gmail.com>
2013-04-08 07:43:01 +0000

    headaches x100

--- A3/src/model/IView.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface IView {
-	/**
-	 * Update the view. This method is generally called by the model whenever it
-	 * has changed state.
-	 */
-	public void updateView();
-}
